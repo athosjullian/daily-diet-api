d4f170101404cf928daf95247c457662c1206a9d
Athos Jullian <athosjullian@gmail.com>
2024-03-26 23:58:05 -0300

    chore: create user migration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import { randomUUID } from "node:crypto";
 
 const server = fastify();
 
-server.get("/", async (request, reply) => {
-  const test = await knex('sqlite_schema').select('*')
+// This route should be deleted soon
+server.get("/users/all", async () => {
+  const test = await knex("users").select("*");
 
-  return test
+  return test;
+});
+
+server.post("/users", async (request, reply) => {
+  const { name } = request.body;
+
+  const user = await knex("users")
+    .insert({
+      id: randomUUID(),
+      name,
+    })
+    .returning("*");
+
+  return reply.status(201).send(user);
 });
 
 server
