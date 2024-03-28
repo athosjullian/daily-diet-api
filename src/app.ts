a747e9f3bab0a26030a8f456ff22ac27067b7b9c
Athos Jullian <athosjullian@gmail.com>
2024-03-28 13:54:31 -0300

    fix: change user migrations, chore: register fastify/cookies

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { knex } from "./database";
 import { randomUUID } from "node:crypto";
 import { z } from "zod";
@@ -12,19 +13,32 @@ app.get("/users/all", async () => {
   return test;
 });
 
+app.register(cookie);
+
 app.post("/users", async (request, reply) => {
   const userBodySchema = z.object({
     name: z.string(),
+    email: z.string(),
   });
+
+  let sessionId = request.cookies.sessionId;
+
+  if (!sessionId) {
+    sessionId = randomUUID();
+
+    reply.cookie("sessionId", sessionId);
+  }
 
   const userBody = userBodySchema.parse(request.body);
 
-  const { name } = userBody;
+  const { name, email } = userBody;
 
   const user = await knex("users")
     .insert({
       id: randomUUID(),
       name,
+      email,
+      session_id: sessionId,
     })
     .returning("*");
 
